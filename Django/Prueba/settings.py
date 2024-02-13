c93cbc4d409edfeafa27fcf38d5e3183b881edc9
Kichola <131254158+AlbertXicola@users.noreply.github.com>
2024-02-13 18:39:34 +0100

    Update settings.py
    arreglar path para usar en linux (settings)

--- Django/Prueba/settings.py
+++ Django/Prueba/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'Prueba.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Django/Django_Flask/progrm/templates'],
+        'DIRS': ['Django/Django_Flask/progrm/templates'],
 
         'APP_DIRS': True,
         'OPTIONS': {
